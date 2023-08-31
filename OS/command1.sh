4e230311ae6f07d3ffee424fc2189e67e5e950d1
Akhil-Sharma-26 <asharma1_be22@thapar.edu>
2023-08-31 16:07:50 +0530

    shell

---
+++ OS/command1.sh
@@ -0,0 +1,7 @@
+read a;
+echo "Helllo";
+echo $a;
+# All shell variables are string variables, hence to carry out arithmetic operations use expr command which evaluates arithmetic expressions.
+# Before and at the end of expr keyword use ` (back quote)
+# More than one assignment can be done in a single statement.
+# The ‘*” symbol must be preceded by a \ ,otherwise the shell treats it as a wildcard character for all files in the current directory.
---
+++ OS/help.md
@@ -0,0 +1,10 @@
+1. –gt Greater than
+2. –lt Less thani
+3. –ge Greater than or equal to
+4. –le Less than or equal to
+5. –ne Not equal to
+6. –eq Equal to
+7. –a Logical AND
+8. –o Logical OR
+9. ! Logical NOT
+
